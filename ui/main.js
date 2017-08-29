13b0ed119683d50a3a69ff7e4964007d7b591a87
mdasifmdasif10 <mdasifmdasif10@gmail.com>
2017-08-29 22:35:33 +0530

    right

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var img=document.getElementbyId('mk');
-img.onclick = function(){
-    img.style.marginright='100px';
+var img = document.getElementbyId('mk');
+img.onclick = function() {
+    img.style.marginRight = '100px';
 };
