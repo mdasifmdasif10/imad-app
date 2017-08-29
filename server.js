1ca58e9a24b20841744f2b40dc80a3f66b19f3ec
mdasifmdasif10 <31028295+mdasifmdasif10@users.noreply.github.com>
2017-08-29 17:06:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/article', function(req, res) {
+app.get('/ui/article.html', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 app.get('/artical-two.html', function(req,res){
