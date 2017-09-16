1ec92b52e94554eaba0418e488ab1ed9bae04669
mdasifmdasif10 <mdasifmdasif10@gmail.com>
2017-09-16 13:36:45 +0530

    ma

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,18 @@ function moveRight() {
 img.onclick = function() {
     var interval = setInterval(moveRight, 50)
 };
+//submit button
+var nameInuput = document.getElementById('naam');
+var naam = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //make a request
+    //name list
+    var naam = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i = 0; i < naam.length; i++) {
+        list += '<li>' + naam[i] + '</li>';
+    };
+    var ul = document.getElementById('naamlist');
+    ul.innerHTML = list;
+};
