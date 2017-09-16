d3a16159849fc139ae3c5433c60478100a015370
mdasifmdasif10 <mdasifmdasif10@gmail.com>
2017-09-16 11:57:27 +0530

    im

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content={
-     title:'article one',
-   heading:'article one',
-      date:'26 jan 2017',
-   content:`'  
+var content = {
+    title: 'article one',
+    heading: 'article one',
+    date: '26 jan 2017',
+    content: `'  
            <p>
                 this is the artical i have writen for the first time with the help of the imad tech. This artical is not to shoe you, its for me to examine This artical is not to shoe you, its for me to examine This artical is not to shoe you, its for me to examine 
             </p>
@@ -23,27 +23,30 @@ var content={
 
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/ui/image.jpg', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'image.jpg'));
 });
 
 app.get('/ui/article.html', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
-app.get('/artical-two.html', function(req,res){
-   res.send('request is made for the artical-two to be display'); 
+app.get('/artical-two.html', function(req, res) {
+    res.send('request is made for the artical-two to be display');
 });
-app.get('/artical-three.html', function(req,res){
-   res.send('request is made for the artical-three to be display'); 
+app.get('/artical-three.html', function(req, res) {
+    res.send('request is made for the artical-three to be display');
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/main.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/madi.png', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
@@ -51,6 +54,6 @@ app.get('/ui/madi.png', function (req, res) {
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+app.listen(port, function() {
+    console.log(`IMAD course app listening on port ${port}!`);
 });
