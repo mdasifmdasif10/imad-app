c74029307e363eff52c059b44545e6a2403738f6
mdasifmdasif10 <mdasifmdasif10@gmail.com>
2017-09-23 15:45:16 +0530

    chg

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
+var config = {
+    user = 'mdasifmdasif10',
+    database = 'mdasifmdasif10',
+    host = 'db.imad.hasura-app.io',
+    port = '5432',
+    password = process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
-        title: 'article one',
-        heading: 'article one',
-        date: '26 jan 2017',
-        content: `'  
-           
-};
-
-
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res) {
+    var hashstring = hash(req.params.input, 'this-is-some-ramdon-string')
+    res.send(hashstring)
+});
+
 app.get('/ui/image.jpg', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'image.jpg'));
 });
